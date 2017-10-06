23bdca0729bcc6b5a6d027f08e687afe5937451b
Giovanna Oruro <orurog@gmail.com>
2017-10-06 18:30:41 -0500

    Estilos finales

--- app.jsx
+++ app.jsx
@@ -39,12 +39,12 @@ class Teachers extends React.Component {
           <li className="teacher">
             <img className="teacher-img" src="https://www.creativosonline.org/blog/wp-content/uploads/2016/06/freelance-gif.png" alt=""/>
             <h2>Teacher 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ad harum tenetur officiis quisquam eius maiores animi hic ipsum impedit perferendis nesciunt, rerum earum odit fugiat laborum numquam eveniet placeat?</p>
+            <p>'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps.</p>
           </li>
           <li className="teacher">
             <img className="teacher-img" src="https://blog.getmagic.com/wp-content/uploads/2017/05/freelancer-hire-1.jpg" alt=""/>
             <h2>Teacher 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ad harum tenetur officiis quisquam eius maiores animi hic ipsum impedit perferendis nesciunt, rerum earum odit fugiat laborum numquam eveniet placeat?</p>
+            <p>Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!</p>
           </li>
           <li className="teacher">
             <img className="teacher-img" src="https://www.researchsnipers.com/wp-content/uploads/2017/02/How-to-Become-a-Successful-Pakistani-Freelancer.jpg" alt=""/>
@@ -77,62 +77,110 @@ render() {
 		const {route} = this.props;
 
 		let HTMLrepos = [{
-			title: "HTML FORMS",
-			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			title: "How to Make a Website",
+			description: "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web.",
 			img_html: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
 			
 		},
 		{
 			title: "HTML FORMS",
-			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_html: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			description: "The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis.",
+			img_html: "https://achievement-images.teamtreehouse.com/badges_html_forms_stage1.png",
 			
 		},
-		
 		{
-			title: "HTML FORMS",
-			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_html: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			title: "HTML Video and Audio",
+			description: "Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text.",
+			img_html: "https://achievement-images.teamtreehouse.com/badges-html-videoaudio-stage1.png",
 			
-		}
+		},
+		{
+			title: "SVG Basics",
+			description: "Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density.",
+			img_html: "https://achievement-images.teamtreehouse.com/badges-html-svg-stage1.png",
+			
+		},
+		{
+			title: "Responsive Images",
+			description: "Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible.",
+			img_html: "https://achievement-images.teamtreehouse.com/badges_html_respimages_stage2.png",
+			
+		},
+		{
+			title: "Introduction to HTML and CSS",
+			description: "Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like.",
+			img_html: "https://achievement-images.teamtreehouse.com/badges_intro_to_html_css_stage-01.png",
+			
+		},
 		];
 
 		let CSSrepos = [{
 			title: "CSS Basic",
-			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_css: "http://www.freelancer.com/static/css/images/logo.jpg",
+			description: "CSS (Cascading Style Sheets) is a style sheet language that describes the presentation of web pages. Whereas HTML is what forms the structure of a web page, CSS is what we use to style the HTML with colors, backgrounds, font sizes, layout, and more. As you'll soon learn, CSS is one of the core technologies for designing and building websites.",
+			img_css: "https://achievement-images.teamtreehouse.com/badges_css_basics_stage6.png",
 			
 		},
 		{
-			title: "CSS Basic",
-			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_css: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			title: "CSS Selectors",
+			description: "In this course, we're going to go beyond the basic selector concepts covered in CSS Basics. Besides the common ways to select elements with type, ID and class selectors, we're able to target elements based on their attributes, position in the HTML document, even their relation to other elements.",
+			img_css: "https://achievement-images.teamtreehouse.com/badges_css_selectors_stage01.png",
 			
 		},
 		
 		{
-			title: "CSS Basic",
+			title: "Responsive Layouts",
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_css: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			img_css: "https://achievement-images.teamtreehouse.com/badges-CSS-ResponsiveLayouts-stage3.png",
+			
+		},
+		{
+			title: "CSS Flexbox Layout",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			img_css: "https://achievement-images.teamtreehouse.com/badges_css_flexbox_layout_stage1.png",
+			
+		},
+		{
+			title: "CSS Transitions and Transforms",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			img_css: "https://achievement-images.teamtreehouse.com/css-transitions-and-transforms-badge-02.png",
+			
+		},
+		{
+			title: "Bootstrap 4 Basics",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			img_css: "https://achievement-images.teamtreehouse.com/badge_bootstrap-4-basics_stage01.png",
 			
 		}
+
 		];
 
 		let JSrepos = [{
 			title: "JAVASCRIPT Basic",
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_js: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			img_js: "https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage1.png",
 			
 		},
 		{
-			title: "JAVASCRIPT Basic",
+			title: "JavaScript Loops, Arrays and Objects",
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
-			img_js: "https://achievement-images.teamtreehouse.com/bagdes_html_howtobuildawebsite_stage02.png",
+			img_js: "https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage5.png",
 			
 		},
 		
 		{
-			title: "JAVASCRIPT Basic",
+			title: "jQuery Basics",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			img_js: "https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage5.png",
+		
+		},
+		{
+			title: "jQuery Basics",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
+			img_js: "https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage5.png",
+		
+		},
+		{
+			title: "jQuery Basics",
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate non quia, nulla dolorem reprehenderit laboriosam officiis nemo provident odit, aspernatur animi! Accusantium iste nobis neque sit minus dicta, quisquam magnam.",
 			img_js: "https://achievement-images.teamtreehouse.com/badges_JavaScript_Basics_Stage5.png",
 		
@@ -146,11 +194,11 @@ render() {
 
 			case 'css':
 				CurrentList = CSSrepos.map( (item, index) => {
-					return <div >
-							<li key = {index} className="course media-group" id="repos">
+					return <div id="content">
+							<li key = {index} className="course media group" id="repos">
 									<img className= "course-img" src={item.img_css} alt=""/>
 								<div >
-									<h2>{item.title}</h2>
+									<h2>{item.title}</h2><br/><br/><br/>
 									<p>{item.description} </p>
 								</div>
 						 	</li>
@@ -159,11 +207,11 @@ render() {
 				break;
 			case 'javascript':
 				CurrentList = JSrepos.map( (item, index) => {
-					return <div>
-							<li key = {index} className="course media-group" id="repos">
+					return <div id="content">
+							<li key = {index} className="course media group" id="repos">
 								<img className= "course-img" src={item.img_js} alt=""/>
 								<div>
-									<h2>{item.title}</h2>
+									<h2>{item.title}</h2><br/><br/><br/>
 									<p>{item.description} </p>
 								</div>
 						 	</li>
@@ -173,13 +221,15 @@ render() {
 				break;
 			default: //'html'
 				CurrentList = HTMLrepos.map( (item, index) => {
-					return <li key = {index} className="course media-group" id="repos"> 
+					return <div id="content">
+							<li key = {index} className="course media group" id="repos">
 								<img className= "course-img" src={item.img_html} alt=""/>
 								<div>
 									<h2>{item.title}</h2><br/><br/><br/>
 									<p>{item.description} </p>
 								</div>
 						 </li>
+					</div>
 				});
 				break;
 		}
